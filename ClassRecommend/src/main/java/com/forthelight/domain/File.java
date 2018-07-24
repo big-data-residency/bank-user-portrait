f6c3ae23734cde2cac659b8b0d8131f4be03a0e1
CielChan <2524497642@qq.com>
2018-07-24 15:25:40 +0800

    添加domain

---
+++ ClassRecommend/src/main/java/com/forthelight/domain/File.java
@@ -0,0 +1,14 @@
+package com.forthelight.domain;
+
+import java.sql.Timestamp;
+
+public class File {
+	private Integer id;
+	private String fileName;
+	private String filePath;
+	private Timestamp updateTime;
+	private Student student;
+	private int deleteStatus;
+	
+	private Course course;
+}
---
+++ ClassRecommend/src/main/java/com/forthelight/domain/StudentCommentCourse.java
@@ -0,0 +1,14 @@
+package com.forthelight.domain;
+
+public class StudentCommentCourse {
+	private Integer id;
+	private String comment;
+	private int gradeScore;
+	private int contentScore;
+	private int likeNumber;
+	private int deleteStatus;
+	
+	private Student student;
+	private Course course;
+
+}
