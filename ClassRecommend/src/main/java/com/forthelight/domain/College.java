34a51e617fc97a326313fcb9cf04fa0c8bbf4e58
王施超 <982030727@qq.com>
2018-07-24 15:45:40 +0800

    添加被删掉的实体类

---
+++ ClassRecommend/src/main/java/com/forthelight/domain/College.java
@@ -0,0 +1,75 @@
+package com.forthelight.domain;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+public class College implements Serializable {
+    private Integer id;
+    private String collegeName;
+    private Integer deleteStatus;
+
+    private Set<Major> majors = new HashSet<>();
+    private Set<Student> students = new HashSet<>();
+    private Set<Teacher> teachers = new HashSet<>();
+    private Set<Course> courses = new HashSet<>();
+
+    public College() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCollegeName() {
+        return collegeName;
+    }
+
+    public void setCollegeName(String collegeName) {
+        this.collegeName = collegeName;
+    }
+
+    public Integer getDeleteStatus() {
+        return deleteStatus;
+    }
+
+    public void setDeleteStatus(Integer deleteStatus) {
+        this.deleteStatus = deleteStatus;
+    }
+
+    public Set<Major> getMajors() {
+        return majors;
+    }
+
+    public void setMajors(Set<Major> majors) {
+        this.majors = majors;
+    }
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
+
+    public Set<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    public void setTeachers(Set<Teacher> teachers) {
+        this.teachers = teachers;
+    }
+
+    public Set<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Set<Course> courses) {
+        this.courses = courses;
+    }
+}
