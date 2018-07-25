2934f9b31d5426891b991fa45b871a67e3386a03
CielChan <2524497642@qq.com>
2018-07-25 12:06:34 +0800

    add mapper version(1.1)

---
+++ ClassRecommend/src/main/java/com/forthelight/dao/CollegeDao.java
@@ -0,0 +1,17 @@
+package com.forthelight.dao;
+
+import java.util.List;
+
+import com.forthelight.domain.College;
+
+public interface CollegeDao {
+	College findById(int id);
+	College findByName(String collegeName);
+	List<College> findByType(int type);
+	List<College> findAll();
+	
+	int update(College college);
+	int delete(int id);
+	int insert(College college);
+
+}
