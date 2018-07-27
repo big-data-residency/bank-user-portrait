697513213e88c04c49714cdf5d0b86603cd7326e
王施超 <982030727@qq.com>
2018-07-27 15:15:05 +0800

    json 传输中文

--- ClassRecommend/src/main/java/com/forthelight/controller/TeacherController.java
+++ ClassRecommend/src/main/java/com/forthelight/controller/TeacherController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 import java.util.List;
@@ -25,6 +26,7 @@ public class TeacherController {
     }
 
     @RequestMapping("/teacherInfo/{teacherId}")
+    @ResponseBody
     public String getTeacherInfo(@PathVariable Integer teacherId){
         Gson gson = new Gson();
         Map<String, String> rsp = new HashMap<>();
