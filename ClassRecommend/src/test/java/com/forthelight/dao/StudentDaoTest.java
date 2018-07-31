5e996f4701cbb0b9019b0d3149845e591001ed9a
CielChan <2524497642@qq.com>
2018-07-31 23:40:58 +0800

    manageStudent

--- ClassRecommend/src/test/java/com/forthelight/dao/StudentDaoTest.java
+++ ClassRecommend/src/test/java/com/forthelight/dao/StudentDaoTest.java
@@ -18,53 +18,77 @@ public class StudentDaoTest {
     @Autowired
     private StudentDao studentDao;
 
+//    @Test
+//    public void findById() {
+//        Student student = studentDao.findById(1);
+//        System.out.println("======");
+//    }
+//
+//    @Test
+//    public void findByMajorId() {
+//        studentDao.findByMajorId(1);
+//
+//    }
+//
+//    @Test
+//    public void insert() {
+//        Student student = new Student();
+//        student.setStudentName("TestStudent");
+//        studentDao.insert(student);
+//
+//    }
+//
+//    @Test
+//    public void findByCollegeId() {
+//        studentDao.findByCollegeId(1);
+//    }
+//
+//    @Test
+//    public void findByStudentNumber() {
+//        Student student = studentDao.findByStudentNumber("1611111");
+//        List<Course> courses = student.getCourses();
+//        Gson gson = new Gson();
+//        System.out.println(gson.toJson(student));
+//        System.out.println(gson.toJson(courses));
+//
+//    }
+//
+//    @Test
+//    public void findByName() {
+//        Student student = studentDao.findByName("蝙蝠侠");
+//        System.out.println(student.getStudentName());
+//    }
+//    
+//    @Test
+//	public void Testupdate() {
+//		Student student = new Student();
+//		student.setId(4);
+//		student.setNickName("hhh");
+//		int result = studentDao.update(student);
+//		System.out.println(result);
+//	}
+    
     @Test
-    public void findById() {
-        Student student = studentDao.findById(1);
-        System.out.println("======");
-    }
-
-    @Test
-    public void findByMajorId() {
-        studentDao.findByMajorId(1);
-
-    }
-
-    @Test
-    public void insert() {
-        Student student = new Student();
-        student.setStudentName("TestStudent");
-        studentDao.insert(student);
-
-    }
-
-    @Test
-    public void findByCollegeId() {
-        studentDao.findByCollegeId(1);
-    }
-
-    @Test
-    public void findByStudentNumber() {
-        Student student = studentDao.findByStudentNumber("1611111");
-        List<Course> courses = student.getCourses();
-        Gson gson = new Gson();
-        System.out.println(gson.toJson(student));
-        System.out.println(gson.toJson(courses));
-
-    }
-
-    @Test
-    public void findByName() {
-        Student student = studentDao.findByName("蝙蝠侠");
-        System.out.println(student.getStudentName());
+    public void TestfindAll() {
+    	List<Student> students = studentDao.findAll();
+    	for(Student student : students) {
+    		System.out.println(student.getStudentName());
+    	}
+    	
     }
     
     @Test
-	public void Testupdate() {
-		Student student = new Student();
-		student.setId(4);
-		student.setNickName("hhh");
-		int result = studentDao.update(student);
-		System.out.println(result);
-	}
+    public void TestfindByKeyword() {
+    	List<Student> students = studentDao.findByKeyword("计算机");
+    	for(Student student : students) {
+    		
+    		System.out.println(student.getStudentName());
+    	}
+    }
+    
+    @Test
+    public void Testdelete() {
+    	int result = studentDao.delete(8);
+    	System.out.println(result);
+    }
 }
