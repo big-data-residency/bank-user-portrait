71c0d1fe2901f21af31d2a61930382474501eb45
王施超 <982030727@qq.com>
2018-07-31 13:31:04 +0800

    多文件上传模板完成
    多文件上传完成 在/view/temp/file_upload.html

--- ClassRecommend/src/main/java/com/forthelight/controller/FileController.java
+++ ClassRecommend/src/main/java/com/forthelight/controller/FileController.java
@@ -34,8 +34,7 @@ public class FileController {
                 // 裁剪图片
                 ImageSize imageSize = gson.fromJson(request.getParameter("avatar_data"), ImageSize.class);
                 BufferedImage image = ImageIO.read(file.getInputStream()).getSubimage(imageSize.getIntX(), imageSize.getIntY(), imageSize.getIntHeight(), imageSize.getIntHeight());
-                // 生成目标文件
-                String fileExt = fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length());
+                // 生成目标文件夹
                 String dstPath = request.getServletContext().getRealPath("images" + File.separator + "avatar");
                 File dstFolder = new File(dstPath);
                 if (!dstFolder.exists()) {
@@ -44,7 +43,9 @@ public class FileController {
                 } else {
                     rsp.put("mkdirs", false);
                 }
+                // 生成目标空文件
                 String dstFileName = fileName.substring(0, fileName.lastIndexOf("."));
+                String fileExt = fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length());
                 File dstFile = new File(new File(dstPath) + File.separator + dstFileName + "." + fileExt);
                 // 拷贝文件
                 ImageIO.write(image, fileExt, dstFile);
@@ -62,6 +63,48 @@ public class FileController {
 
 
 
+
+        return gson.toJson(rsp);
+    }
+
+    @RequestMapping(value = "/fileUpload", produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public String fileUpload(@RequestParam("files") MultipartFile[] files, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Map<String, Object> rsp = new HashMap<>();
+        Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
+
+
+        if(files != null && files.length > 0){
+            for(MultipartFile file: files){
+                if(file!=null && file.getSize() > 0){
+                    String fileName = file.getOriginalFilename();
+                    if(fileName != null) {
+                        // 生成目标文件夹
+                        String dstPath = request.getServletContext().getRealPath("files");
+                        File dstFolder = new File(dstPath);
+                        if (!dstFolder.exists()) {
+                            dstFolder.mkdirs();
+                            rsp.put("mkdirs", true);
+                        } else {
+                            rsp.put("mkdirs", false);
+                        }
+                        // 生成目标空文件
+                        File dstFile = new File(new File(dstPath) + File.separator + fileName);
+                        FileUtils.copyToFile(file.getInputStream(), dstFile);
+                        rsp.put("code", 200);
+                        rsp.put("success", true);
+                    }
+                } else {
+                    rsp.put("code", 404);
+                    rsp.put("success", false);
+                    rsp.put("errorInfo", "empty file");
+                }
+            }
+        } else {
+            rsp.put("code", 404);
+            rsp.put("success", false);
+            rsp.put("errorInfo", "no file accept");
+        }
 
         return gson.toJson(rsp);
     }
