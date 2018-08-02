b90f852955674ceb868e53aa38079f5bd7a77db8
王施超 <982030727@qq.com>
2018-08-02 18:18:27 +0800

    选课界面做了一点点修改

--- ClassRecommend/src/main/webapp/style/newstyle/js/star.js
+++ ClassRecommend/src/main/webapp/style/newstyle/js/star.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   
   /* 1. Visualizing things on Hover - See next part for action on click */
-  $('#stars li').on('mouseover', function(){
+  $('.stars li').on('mouseover', function(){
     var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
    
     // Now highlight all the stars that's not after the current hovered star
@@ -22,7 +22,7 @@ $(document).ready(function(){
   
   
   /* 2. Action to perform on click */
-  $('#stars li').on('click', function(){
+  $('.stars li').on('click', function(){
     var onStar = parseInt($(this).data('value'), 10); // The star currently selected
     var stars = $(this).parent().children('li.star');
     
@@ -35,7 +35,7 @@ $(document).ready(function(){
     }
     
     // JUST RESPONSE (Not needed)
-    var ratingValue = parseInt($('#stars li.selected').last().data('value'), 10);
+    var ratingValue = parseInt($('.stars li.selected').last().data('value'), 10);
     var msg = "";
     if (ratingValue > 1) {
         msg = "Thanks! You rated this " + ratingValue + " stars.";
